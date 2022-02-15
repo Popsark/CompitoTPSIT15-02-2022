d8f291eb765704a09f5087d1f1c8fd2fc77d3175
lai2-01 <lai2-01@itismeucci.com>
2022-02-15 11:29:14 +0100

    Primo commit

---
+++ src/Ciclo.java
@@ -0,0 +1,10 @@
+public class  Ciclo{
+    public static void main(String[] args) throws Exception {
+        int cont=0;
+
+        for(int i=0; i<10; i++){
+            System.out.println(cont);
+            cont++;
+        }
+    }
+}
